beead02e7341c4d9c3383f72d35dc26d3bb9004c
ZEUSOFCS <zeusofohio@gmail.com>
2016-10-04 22:45:52 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+
+/*
+ Author      : DORIAN JAVA BROWN
+ Version     : NULL
+ Copyright   : All Rights Reserve; You may use, distribute and modify this code.
+ Description : Prints "Hello, World". By tradition, this is everyone's first program.
+ */
