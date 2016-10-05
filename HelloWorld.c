d3001f6b73df7cd01e946cb0df3ebd7ac4726c0b
ZEUSOFCS <zeusofohio@gmail.com>
2016-10-04 22:20:11 -0400

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,3 +1,11 @@
+/*
+ Author      : DORIAN JAVA BROWN
+ Version     : NULL
+ Copyright   : All Rights Reserve; You may use, distribute and modify this code.
+ Description : Prints "Hello, World". By tradition, this is everyone's first program.
+ */
+
+
 #include <stdio.h>
 
 int main() {
