7cd7d36846f4e30abaecf5c092bb280f3ab5e85d
ZEUSOFCS <zeusofohio@gmail.com>
2016-10-04 23:04:06 -0400

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-
-/*
- Author      : DORIAN JAVA BROWN
- Version     : NULL
- Copyright   : All Rights Reserve; You may use, distribute and modify this code.
- Description : Prints "Hello, World". By tradition, this is everyone's first program.
- */
