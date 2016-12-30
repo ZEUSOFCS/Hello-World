d032174fa127f5f2c6e21f50a753d60b87354e52
ZEUSOFCS <zeusofohio@gmail.com>
2016-12-29 22:08:33 -0500

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,5 @@
 /*
- Author      : DORIAN JAVA BROWN
+ Author      : DORIAN JAVA BROWN.
  Version     : NULL
  Copyright   : All Rights Reserve; You may use, distribute and modify this code.
  Description : Prints "Hello, World". By tradition, this is everyone's first program.
